28ea38f83bb6f4e2fe13f422f0a3021854925c1b
Brian Webb <bwebb@indatus.com>
2014-02-24 06:26:58 -0500

    require-packages composer function working and tested

--- src/lib/Construction/Composer.php
+++ src/lib/Construction/Composer.php
@@ -3,6 +3,7 @@
 use Illuminate\Filesystem\Filesystem;
 use Console\BuildCommand;
 use Support\Path;
+use Construction\TemplateReader;
 
 /**
  * Class for interacting with / adding to the composer.json
@@ -11,6 +12,25 @@ use Support\Path;
  */
 class Composer
 {
+
+    /**
+     * Constants corresponding to sections of the composer.json
+     * config file
+     */
+    const NAME                        = 'name';
+    const DESCRIPTION                 = 'description';
+    const KEYWORDS                    = 'keywords';
+    const LICENSE                     = 'license';
+    const REQUIRE_DEPENDENCIES        = 'require';
+    const REQUIRE_DEV_DEPENDENCIES    = 'require-dev';
+    const AUTOLOAD_CLASSMAP           = 'autoload.classmap';
+    const AUTOLOAD_PSR0               = 'autoload.psr-0';
+    const AUTOLOAD_PSR4               = 'autoload.psr-4';
+    const SCRIPTS_POST_INSTALL        = 'scripts.post-install-cmd';
+    const SCRIPTS_POST_UPDATE         = 'scripts.post-update-cmd';
+    const SCRIPTS_POST_CREATE_PROJECT = 'scripts.post-create-project-cmd';
+    const CONFIG                      = 'config';
+    const MINIMUM_STABILITY           = 'minimum-stability';
 
     /**
      * Filesystem object for interacting with the
@@ -37,6 +57,14 @@ class Composer
     protected $appDir;
 
     /**
+     * Multi-Dimension array of config
+     * settings from the Foreman template
+     * 
+     * @var array
+     */
+    protected $config;
+
+    /**
      * Var to hold a multi-dimensional associative
      * array representing the composer.json configuration
      * directives
@@ -51,13 +79,15 @@ class Composer
      * and read in the composer.json file and convert to a 
      * multi-dimensional array for later operation
      * 
-     * @param string       $appDir
-     * @param Filesystem   $fs    
-     * @param BuildCommand $cmd   
+     * @param string       $appDir absolute path to application
+     * @param array        $config Foreman template config
+     * @param Filesystem   $fs     Filesystem object
+     * @param BuildCommand $cmd    BuildCommand object
      */
-    public function __construct($appDir, Filesystem $fs, BuildCommand $cmd)
+    public function __construct($appDir, array $config, Filesystem $fs, BuildCommand $cmd)
     {
         $this->appDir = $appDir;
+        $this->config = $config;
         $this->filesystem = $fs;
         $this->command = $cmd;
 
@@ -78,5 +108,69 @@ class Composer
     public function getComposerArray()
     {
         return $this->composer;
+    }
+
+
+    // public function getComposerJson()
+    // {
+    //     return json_encode($this->composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    // }
+
+
+    // public function writeComposerJson()
+    // {
+    //     (new Filesystem)->put('/Users/bwebb/Desktop/composer.json', $this->getComposerJson());
+    // }
+
+
+    public function requirePackages()
+    {
+        $key = str_replace(TemplateReader::COMPOSER.'.', '', TemplateReader::COMPOSER_REQ);
+
+        $data = array_get($this->config, $key);
+        $require = [];
+
+        foreach ($data as $package) {
+            $pkg = $package['package'];
+            $ver = $package['version'];
+
+            $this->command->comment("Composer", "Require {$pkg} {$ver}");
+
+            $require[$pkg] = $ver;
+        }
+
+        $this->composer[static::REQUIRE_DEPENDENCIES] = $require;
+    }
+
+
+    public function requireDevPackages()
+    {
+        $key = str_replace(TemplateReader::COMPOSER.'.', '', TemplateReader::COMPOSER_REQ_DEV);
+
+        $data = array_get($this->config, $key);
+    }
+
+
+    public function autoloadClassmap()
+    {
+        $key = str_replace(TemplateReader::COMPOSER.'.', '', TemplateReader::COMPOSER_AUTOLOAD_CLS_MAP);
+
+        $data = array_get($this->config, $key);
+    }
+
+
+    public function autoloadPsr0()
+    {
+        $key = str_replace(TemplateReader::COMPOSER.'.', '', TemplateReader::COMPOSER_AUTOLOAD_PSR0);
+
+        $data = array_get($this->config, $key);
+    }
+
+
+    public function autoloadPsr4()
+    {
+        $key = str_replace(TemplateReader::COMPOSER.'.', '', TemplateReader::COMPOSER_AUTOLOAD_PSR4);
+
+        $data = array_get($this->config, $key);
     }
 }
--- tests/Construction/ComposerTest.php
+++ tests/Construction/ComposerTest.php
@@ -30,6 +30,7 @@ class ComposerTest extends \PHPUnit_Framework_TestCase
 
         $composer = new Composer(
             $appDir,
+            $this->getConfig(),
             $mFS,
             $mCmd
         );
@@ -38,6 +39,86 @@ class ComposerTest extends \PHPUnit_Framework_TestCase
             json_decode($this->getComposerJson(), true),
             $composer->getComposerArray()
         );
+    }
+
+
+
+    public function testRequirePackages()
+    {
+        $appDir = '/path/to/app';
+        $composerPath = $appDir.DIRECTORY_SEPARATOR.'composer.json';
+        $require = $this->getConfig()['require'];
+
+        $mFS = m::mock('Illuminate\Filesystem\Filesystem');
+        $mFS->shouldReceive('get')
+            ->once()
+            ->with($composerPath)
+            ->andReturn($this->getComposerJson());
+   
+        $mCmd = m::mock('Console\BuildCommand');
+        $mCmd->shouldIgnoreMissing();
+
+        foreach ($require as $package) {
+
+            $pkg = $package['package'];
+            $ver = $package['version'];
+
+            $mCmd->shouldReceive('comment')
+                ->once()
+                ->with("Composer", "Require {$pkg} {$ver}");
+        }
+        
+
+        $composer = new Composer(
+            $appDir,
+            $this->getConfig(),
+            $mFS,
+            $mCmd
+        );
+        $composer->requirePackages();
+
+        $expectedPackages = [
+            'laravel/framework'  => '4.1.*',
+            'nesbot/Carbon'      => '*',
+            'doctrine/inflector' => '1.0.*@dev'
+        ];
+
+        $this->assertEquals(
+            $expectedPackages,
+            $composer->getComposerArray()[Composer::REQUIRE_DEPENDENCIES]
+        );
+
+    }
+
+
+    private function getConfig()
+    {
+        return [
+            'require' => [
+                ['package' => 'laravel/framework', 'version' => '4.1.*'],
+                ['package' => 'nesbot/Carbon', 'version' => '*'],
+                ['package' => 'doctrine/inflector', 'version' => '1.0.*@dev'],
+            ],
+            'require-dev' => [
+                ['package' => 'mockery/mockery', 'version' => 'dev-master@dev'],
+                ['package' => 'fzaninotto/faker', 'version' => '1.3.*'],
+                ['package' => 'squizlabs/php_codesniffer', 'version' => '*'],
+            ],
+            'autoload' => [
+                'classmap' => [
+                    'app/commands',
+                    'app/controllers',
+                    'app/models',
+                    'app/database/migrations',
+                    'app/database/seeds',
+                    'app/test/TestCase.php'
+                ],
+                'psr-0' => [
+                    "Acme" => "app/lib"
+                ],
+                'psr-4' => []
+            ]
+        ];
     }
 
 
